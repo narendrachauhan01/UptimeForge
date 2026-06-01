29b5c4184f5745192b4e50e6e11d057a61fa2542
Narendra Singh <narendra@addwebsolution.in>
2026-06-01 16:14:46 +0530

    Set user login rate limit to 30

--- backend/routes/userAuth.js
+++ backend/routes/userAuth.js
@@ -5,7 +5,7 @@ const rateLimit = require('express-rate-limit');
 
 const loginLimiter = rateLimit({
     windowMs: 15 * 60 * 1000,
-    max: 40,
+    max: 30,
     message: { error: 'Too many login attempts. Try again after 15 minutes.' },
     standardHeaders: true, legacyHeaders: false,
 });
